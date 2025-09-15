541f95152bc495470ec708d45a5b6c114a5522e6
Mareedu Saibabu <a3097640@bestbuy.com>
2025-09-15 19:47:03 +0530

    ArrayList

--- packages/collections/src/abstracts/AbstractList.ts
+++ packages/collections/src/abstracts/AbstractList.ts
@@ -3,4 +3,6 @@ import { List } from '../interfaces/List.js';
 
 export abstract class AbstractList<T> implements List<T> {
   abstract iterator(): Iterator<T>;
+  abstract add(element: T): void;
+  abstract size(): number;
 }
